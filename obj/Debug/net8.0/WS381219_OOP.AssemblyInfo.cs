1e0fea8e524e741a6f262922ceba0e4323b03250
jasmine-smith8 <jasmine.smith@hpe.com>
2024-11-22 15:50:53 +0000

    update csv

--- obj/Debug/net8.0/WS381219_OOP.AssemblyInfo.cs
+++ obj/Debug/net8.0/WS381219_OOP.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WS381219_OOP")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f5ec47409371975c9ce1664011e79e6ca60c01b2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8fe70a954ef1a2567199ac11a5c1cde8a7a36b93")]
 [assembly: System.Reflection.AssemblyProductAttribute("WS381219_OOP")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WS381219_OOP")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
